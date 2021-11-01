b0e03ea56c55fc47aa186ddcfc0c7b8ee9211e7c
kgaru <93502480+kgaru@users.noreply.github.com>
2021-10-31 22:17:37 -0500

    Create EvenNumber.java

---
+++ 01. Patterns/EvenNumber.java
@@ -0,0 +1,17 @@
+// WAP given number is even number or not
+
+package com.flowcontrols;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 7;
+		if (n % 2 == 0) {
+			System.out.println("even number");
+		} else {
+			System.out.println("not a even number");
+		}
+
+	}
+}
